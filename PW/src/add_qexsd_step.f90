b790048f5d6754fbe0a1f0e64f540a6662a4142e
Pietro Delugas <pdelugas@sissa.it>
2017-08-24 13:57:48 +0200

    typo correction

--- PW/src/add_qexsd_step.f90
+++ PW/src/add_qexsd_step.f90
@@ -51,7 +51,7 @@ IF ( lfcpopt .OR. lfcpdyn ) THEN
                              scf_error, POTSTAT_CONTR = (ef * tot_charge),  FCP_FORCE  = (fcp_mu-ef) ,                &
                              FCP_TOT_CHARGE = tot_charge)
 ELSE 
-   CALL qexsd_step_addstep( i_step, nstep, nsp, atm, ityp, nat, tau, alat, alat*t(:,1), alat*at(:,2), alat*at(:,3), &
+   CALL qexsd_step_addstep( i_step, nstep, nsp, atm, ityp, nat, tau, alat, alat*at(:,1), alat*at(:,2), alat*at(:,3), &
                             etot, eband, ehart, vtxc, etxc, ewld, degauss, demet, force, sigma, n_scf_steps, scf_error)
 END IF 
 #else
