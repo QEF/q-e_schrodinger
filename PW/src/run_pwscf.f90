67d80ff481bbe8fe83c9149eec750c2c1a5367be
Sasha Fonari <fonari@schrodinger.com>
2022-10-17 15:23:11 -0400

    Set correct exit code on SCF failure
    Set Schrodinger version

--- PW/src/run_pwscf.f90
+++ PW/src/run_pwscf.f90
@@ -167,11 +167,14 @@ SUBROUTINE run_pwscf( exit_status )
      ! ... code stopped by user or not converged
      !
      IF ( check_stop_now() .OR. .NOT. conv_elec ) THEN
-        IF ( check_stop_now() ) exit_status = 255
-        IF ( .NOT. conv_elec) THEN
-            IF (dmft) exit_status =  131
+        IF ( check_stop_now() ) THEN
+           exit_status = 255
         ELSE
-            exit_status = 2
+           IF (dmft) THEN
+              exit_status =  131
+           ELSE
+              exit_status = 2
+           ENDIF
         ENDIF
         CALL qexsd_set_status(exit_status)
         CALL punch( 'config' )
